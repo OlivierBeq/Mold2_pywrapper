062ffaffadb8d1595a60214ff4a08b394357cb9a
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2023-06-15 09:41:32 +0200

    Force conversion to numeric values

--- src/Mold2_pywrapper/mold2_wrapper.py
+++ src/Mold2_pywrapper/mold2_wrapper.py
@@ -251,6 +251,7 @@ DOI: 10.1021/ci800038f
             data.columns = data.iloc[0, :]
             data.drop(index=0, inplace=True)
         data.reset_index(drop=True, inplace=True)
+        data = pd.to_numeric(data, errors='coerce')
         data = data.convert_dtypes()
         return data
 
